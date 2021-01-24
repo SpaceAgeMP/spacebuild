2b8a4bfe80b8b780c454e4958073a86ddd86707d
Mark Dietzer <git@doridian.net>
2021-01-24 11:25:22 -0800

    Fix errors

--- lua/caf/core/server/notify.lua
+++ lua/caf/core/server/notify.lua
@@ -1,5 +1,5 @@
 function CAF.NotifyOwner(ent, msg)
     -- TODO: Replace with net msg, this is unsafe!
-    msg = msg.replace("'", "\\'")
+    msg = msg:Replace("'", "\\'")
     ent:GetOwner():SendLua("GAMEMODE:AddNotify('" .. msg .. "', NOTIFY_GENERIC, 7);")
 end
