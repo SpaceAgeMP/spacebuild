bd63ca1971d7e011761e22453d5827710972f74a
Doridian <git@doridian.net>
2024-12-26 16:21:08 -0800

    Redefine constants

--- lua/caf/core/server/notify.lua
+++ lua/caf/core/server/notify.lua
@@ -1,10 +1,22 @@
+-- These are no longer defined on the server, so define them ourselves...
+NOTIFY_GENERIC = 0
+NOTIFY_ERROR = 1
+NOTIFY_UNDO = 2
+NOTIFY_HINT = 3
+NOTIFY_CLEANUP = 4
+
 local PLY = FindMetaTable("Player")
 util.AddNetworkString("Player_AddHint")
 function PLY:AddHint(txt, typ, len)
+    if not txt then
+        error("No text specified for hint")
+        return
+    end
+
     net.Start("Player_AddHint")
         net.WriteString(txt)
-        net.WriteInt(typ, 32)
-        net.WriteInt(len, 32)
+        net.WriteInt(typ or NOTIFY_GENERIC, 32)
+        net.WriteInt(len or 5, 32)
     net.Send(self)
 end
 
